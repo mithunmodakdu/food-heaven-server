4b7d51c29643050d81514eab09c352a5c4ed7249
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-04-06 22:33:56 +0600

    carts get, post, delete endpoints have been created

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// 
-// 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mcynqnr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,14 +28,39 @@ async function run() {
 
     const menuCollection = client.db("foodHeavenDB").collection("menu");
     const reviewsCollection = client.db("foodHeavenDB").collection("reviews");
+    const cartsCollection = client.db("foodHeavenDB").collection("carts");
     
+    // :::: menu :::::
     app.get('/menu', async(req, res)=>{
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
+    // :::: reviews :::::
     app.get('/reviews', async(req, res)=>{
       const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    })
+
+    // ::::: carts ::::
+    app.get('/carts', async(req, res)=>{
+      const email = req.query.email;
+      const query = {email: email}
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/carts', async(req, res)=>{
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
+      res.send(result);
+
+    })
+
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     })
 
