3ddf26117e8407ee54dfbd6df7afdc29462d87cb
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-04-08 21:07:05 +0600

    users get routes has been created

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ async function run() {
     const cartsCollection = client.db("foodHeavenDB").collection("carts");
     
     // :::: users related endpoints :::::
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res)=>{
       const user = req.body;
 
