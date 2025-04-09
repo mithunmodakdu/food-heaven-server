c0972767d30ba30cba5871def671f3e6cd356e19
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-04-09 16:20:42 +0600

    user delete API has been created

--- index.js
+++ index.js
@@ -51,6 +51,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // :::: menu related endpoint :::::
     app.get('/menu', async(req, res)=>{
       const result = await menuCollection.find().toArray();
