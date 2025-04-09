5e88c805ab2d65cdbcca6de04f9be03b13bb9bf6
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-04-09 16:40:11 +0600

    user patch API has been created for updating or making admin role

--- index.js
+++ index.js
@@ -58,6 +58,18 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
     // :::: menu related endpoint :::::
     app.get('/menu', async(req, res)=>{
       const result = await menuCollection.find().toArray();
