fb8973fd823a0e56e573189f2c14fd6471201c39
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-04-18 17:20:38 +0600

    menu delete api has been created

--- index.js
+++ index.js
@@ -135,6 +135,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // :::: reviews related endpoint :::::
     app.get('/reviews', async(req, res)=>{
       const result = await reviewsCollection.find().toArray();
